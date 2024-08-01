10115dff210f6dc8497f65c38f15cea5192888cf
EriclesRafael <ericlesrafael@outlook.com>
2024-08-01 00:11:56 -0300

    completo

--- src/exercicio1/EmprestimoLivro.java
+++ src/exercicio1/EmprestimoLivro.java
@@ -1,22 +1,45 @@
 package exercicio1;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 public class EmprestimoLivro {
 
     public static void main(String[] args) {
+        List<Livro> livros = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
 
-        Livro livro1 = new Livro("1984", "George Orwell", 1949, false);
-        Livro livro2 = new Livro("Fahrenheit 451", "Ray Bradbury", 1953, false);
+        while (true) {
+            System.out.println("Digite o titulo do seu livro (ou sair para encerrar): ");
+            String titulo = scanner.nextLine();
 
-        System.out.println(livro1.toString());
-        System.out.println(livro2.toString());
+            if (titulo.equalsIgnoreCase("sair")) {
+                break;
+            }
 
-        livro1.alterarStatusEmprestimo();
-        System.out.println("Novo status do livro1: " + livro1.isEmprestado());
+            System.out.println("Digite o autor do livro");
+            String autor = scanner.nextLine();
 
-        if (livro1.equals(livro2)) {
-            System.out.println("Os livros são iguais.");
-        } else {
-            System.out.println("Os livros são diferentes.");
+            System.out.println("Digite o ano de publicação do livro");
+            int anoPublicacao = 0;
+            try {
+                anoPublicacao = Integer.parseInt(scanner.nextLine());
+            } catch (NumberFormatException e) {
+                System.out.println("Erro: O ano de publicação deve ser um inteiro.");
+                continue;
+            }
+
+            System.out.println("O livro está emprestado? (true/false): ");
+            boolean emprestado = Boolean.parseBoolean(scanner.nextLine());
+
+            Livro livro = new Livro(titulo, autor, anoPublicacao, emprestado);
+            livros.add(livro);
         }
+
+        System.out.println("Lista de livros");
+        for (Livro livro : livros) {
+            System.out.println(livro.toString());
+        }
+        scanner.close();
     }
 }
